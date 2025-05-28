c581359323016dd8c7cb0b04c5eeb4fcae792110
ilafper <ialfper@aulaestudio.es>
2025-05-28 18:16:16 +0200

    espe

--- api/app.js
+++ api/app.js
@@ -80,9 +80,9 @@ app.post('/api/registrarse', async (req, res) => {
 
     // Crear el nuevo especialista
     const nuevoUser = {
-      nombre,
-      email,
-      password1
+      nombre:nombre,
+      email:email,
+      contrasena:password1
     };
 
     await login.insertOne(nuevoUser);
