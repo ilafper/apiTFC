19ea879fdc4dc412bfeed6188804dd67ede86807
ilafper <ialfper@aulaestudio.es>
2025-12-04 01:39:56 +0100

    pataa

--- api/app.js
+++ api/app.js
@@ -37,12 +37,6 @@ async function connectToMongoDB() {
   }
 }
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 app.post('/api/checkLogin', async (req, res) => {
   try {
