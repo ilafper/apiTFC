2cb37519677d96efcf0907f50735357a2a7ceb50
ilafper <ialfper@aulaestudio.es>
2025-12-04 01:23:25 +0100

    efectivamente lo soy

--- api/app.js
+++ api/app.js
@@ -7,10 +7,11 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:3000'],
+  origin: '*',  // <-- EL ASTERISCO PERMITE TODOS
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 const uri = "mongodb+srv://ialfper:ialfper21@alumnos.zoinj.mongodb.net/alumnos?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
