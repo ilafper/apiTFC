92d3e20a7a839d9ac0541138ff0052c9c14c7c0d
ilafper <ialfper@aulaestudio.es>
2025-12-04 01:23:44 +0100

    lo reconfirmo

--- api/app.js
+++ api/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 
 app.use(cors({
-  origin: '*',  // <-- EL ASTERISCO PERMITE TODOS
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
